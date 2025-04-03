6704d968dcad4cadfdc51a0673d7c83cdffb020c
Masterhmh <masterhmh@gmail.com>
2025-04-03 11:44:16 +0700

    Update worker.js

--- worker.js
+++ worker.js
@@ -4,10 +4,8 @@ addEventListener('fetch', event => {
 });
 
 async function handleRequest(request) {
-  // URL của GAS Web App
   const targetUrl = 'https://script.google.com/macros/s/AKfycbxBu0eH5pYLGb4kPx03GhcYWL6QIP5LChAVRBLeAYx1I6QugGphj2iCzV29_000/exec';
 
-  // Xử lý yêu cầu OPTIONS (preflight)
   if (request.method === 'OPTIONS') {
     return new Response(null, {
       headers: {
@@ -19,7 +17,6 @@ async function handleRequest(request) {
     });
   }
 
-  // Chuyển tiếp yêu cầu đến GAS
   const modifiedRequest = new Request(targetUrl, {
     method: request.method,
     headers: request.headers,
@@ -28,7 +25,6 @@ async function handleRequest(request) {
 
   const response = await fetch(modifiedRequest);
 
-  // Thêm header CORS vào response
   const newHeaders = new Headers(response.headers);
   newHeaders.set('Access-Control-Allow-Origin', 'https://test-miniapp.netlify.app');
   newHeaders.set('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
